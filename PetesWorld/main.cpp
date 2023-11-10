f6cf14618b1d15078cd04c14c6bbb27764cd5464
(Student  C00299407) Kacie Franklin <C00299407@setu.ie>
2023-11-10 12:34:23 +0000

    X

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to A or Y to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to A or X to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{
@@ -77,7 +77,29 @@ int main()
 		{
 			std::cout << "Yemen Arabian Pennisula" << std::endl;
 		}
-
+	}
+	if (letter == 'x' || letter == 'X') // written by Kacie
+	{
+		if (region == 1)
+		{
+			std::cout << "There are mo xs in the Americas" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are mo xs in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are mo xs in Africa" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are mo xs in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are mo xs in the Rest of the world" << std::endl;
+		}
 	}
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
